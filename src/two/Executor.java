21870b0afb5667ac5d4b9bb0c8c551418688ecc1
iras1114 <disoturko@gmail.com>
2022-01-20 13:11:42 +0100

    Moje Prve Vjezbice 1

---
+++ src/two/Executor.java
@@ -0,0 +1,23 @@
+package two;
+
+public class Executor {
+    public static void main(String[] args) {
+        // Tip varijabla = podatak
+        char slovo = 'E';
+        System.out.println(slovo);
+        //Stringovi su složeni tipovi podataka i zauzimaju mnogo memorije
+        //Strinovi su niz karaktera
+        // Stringovi se mogu pisati i na sljedeći način:
+        // String name = new String( original: "Teodora");
+        String name = "Teodora";
+        String surname = "Bajić";
+        String name1 = "Teodora";
+
+
+
+
+
+
+
+    }
+}
